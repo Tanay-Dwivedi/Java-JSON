6644938cc9132493e88453d1943bcb3d9171449b
Tanay-Dwivedi <91426152+Tanay-Dwivedi@users.noreply.github.com>
2023-11-29 12:29:37 +0530

    java to JSON

--- src/main/java/javajson/Jsoning.java
+++ src/main/java/javajson/Jsoning.java
@@ -1,7 +1,53 @@
 package javajson;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Jsoning {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        try {
+            // Create an ObjectMapper to read and write JSON
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            // Read existing data from the file as a List<Person>
+            List<Person> existingPeople;
+
+            // Specify the file path for the JSON data
+            File file = new File("target/personData.json");
+
+            // Check if the file is not empty
+            if (file.exists() && file.length() > 0) {
+                existingPeople = objectMapper.readValue(file, new TypeReference<List<Person>>() {
+                });
+            } else {
+                // If the file is empty or doesn't exist, initialize an empty list
+                existingPeople = new ArrayList<>();
+            }
+
+            // Create new data to add
+            Person newPerson = new Person(
+                    "Tanay",
+                    "new.person@example.com",
+                    "25",
+                    "1998-01-01",
+                    "+9876543210",
+                    "Canada",
+                    new String[]{"English", "French"}
+            );
+
+            // Add only the new data to the existing data
+            existingPeople.add(newPerson);
+
+            // Write the updated data back to the same file
+            objectMapper.writeValue(file, existingPeople);
+        } catch (final Exception e) {
+            // Handle exceptions, such as JSON parsing or file I/O errors
+            System.out.println(e.getMessage());
+        }
     }
 }
--- src/main/java/javajson/Person.java
+++ src/main/java/javajson/Person.java
@@ -1,11 +1,5 @@
 package javajson;
 
-import lombok.*;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-
 public class Person {
 
     private String name;
@@ -15,5 +9,48 @@ public class Person {
     private String phone_number;
     private String country;
     private String[] languages;
+
+    public Person() {
+        super();
+    }
+
+    public Person(String name, String age, String email, String date_of_birth, String phone_number, String country, String[] languages) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.date_of_birth = date_of_birth;
+        this.phone_number = phone_number;
+        this.country = country;
+        this.languages = languages;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getDate_of_birth() {
+        return date_of_birth;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String[] getLanguages() {
+        return languages;
+    }
+
 
 }
